6450c73ecf6b84496b498aa716e8b2928ed19f21
154773 <saravanan6.kumar6@cognizant.com>
2019-12-29 04:43:04 +0000

    Setting up the layout using databinding

--- app/src/main/java/codingwithmitch/com/databindinggettingstarted/MainActivity.java
+++ app/src/main/java/codingwithmitch/com/databindinggettingstarted/MainActivity.java
@@ -1,13 +1,21 @@
 package codingwithmitch.com.databindinggettingstarted;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import codingwithmitch.com.databindinggettingstarted.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    //class autocreated when databinding enabled
+    //name of the layout file with hyphens removed and characters autocapitalized
+    ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        
     }
 }
