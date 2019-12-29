5332e0441eb1834ab94709843dc75f9833a757b1
154773 <saravanan6.kumar6@cognizant.com>
2019-12-29 04:46:45 +0000

    Setting up a dynamic string binding to the layout file.

--- app/src/main/java/codingwithmitch/com/databindinggettingstarted/MainActivity.java
+++ app/src/main/java/codingwithmitch/com/databindinggettingstarted/MainActivity.java
@@ -5,17 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import codingwithmitch.com.databindinggettingstarted.databinding.ActivityMainBinding;
+import codingwithmitch.com.databindinggettingstarted.models.Product;
+import codingwithmitch.com.databindinggettingstarted.util.Products;
 
 public class MainActivity extends AppCompatActivity {
     //class autocreated when databinding enabled
     //name of the layout file with hyphens removed and characters autocapitalized
     ActivityMainBinding activityMainBinding;
+    Product product;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
         activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
-        
+        Products productList = new Products();
+        product = productList.PRODUCTS[0];
+
+        //This setter method is the autocreated method for the variable name
+        //created in layout file
+        activityMainBinding.setProduct(product);
     }
 }
