88564ed98090448857dbd9a780da083f438180e8
154773 <saravanan6.kumar6@cognizant.com>
2019-12-29 04:50:26 +0000

    Dynamic binding of int value with string concatination. Requires helper class to do the same.

--- app/src/main/java/codingwithmitch/com/databindinggettingstarted/util/StringUtil.java
+++ app/src/main/java/codingwithmitch/com/databindinggettingstarted/util/StringUtil.java
@@ -1,14 +1,19 @@
 package codingwithmitch.com.databindinggettingstarted.util;
 
 
+/**
+ * Helper class for dynamic substitution of int values in layout file
+ */
 public class StringUtil {
 
     public static String getQuantityString(int quantity){
         return ("Qty: " + String.valueOf(quantity));
     }
 
+    // used to conver the int value to string and append strings to format the value
+    //this concatination cannot be directly done in the xml file.
     public static String convertIntToString(int value){
-        return (String.valueOf(value));
+        return ("("+String.valueOf(value)+")");
     }
 
 
